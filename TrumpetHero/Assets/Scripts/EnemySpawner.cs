14deaca23ec47a92c371537c261a6a122464d331
James Heath <jh230155@falmouth.ac.uk>
2020-03-13 22:20:12 +0000

    Added enemy spawner script.

---
+++ TrumpetHero/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject obstacle;
+    public GameObject[] obstacleNotes;
+    public int note;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        yield return new WaitForSecondsRealtime(2);
+        note = Random.Range(0, 12);
+        Instantiate(obstacle, obstacleNotes[note].transform.position);
+    }
