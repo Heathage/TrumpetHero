ca28b368fd944912474dffc985ab8d35615f3647
James Heath <jh230155@falmouth.ac.uk>
2020-03-16 13:53:36 +0000

    Moved TrebleClef Z pos to -2 Added 2D Box Collider to Treble Clef Added a script to Treble Clef that will destroy game objects when they collide. Renamed EnemySpawner.cs to ObstacleSpawner.cs

---
+++ TrumpetHero/Assets/Scripts/ObstacleDespawner.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleDespawner : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D Obstacle)
+    {
+        Destroy(Obstacle.gameObject);
+    }
+}
---
+++ TrumpetHero/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public GameObject obstacle;
+    public GameObject[] obstacleNotes;
+    public int note;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(Spawn());
+    }
+    private IEnumerator Spawn()
+    {
+        yield return new WaitForSecondsRealtime(2);
+        note = Random.Range(0, 12);
+        Vector3 spawnPos = obstacleNotes[note].transform.position;
+        Debug.Log(spawnPos);
+        //Instantiate(obstacle, spawnPos);
+    }
+}
