14bf82d7ce9e9d8ce456a1b04953b465ebec5c1a
James Heath <jh230155@falmouth.ac.uk>
2020-03-16 16:13:24 +0000

    A difficulty timer has been added. Increasing the difficulty over time. Easy, Medium, and Hard. They decrease the time between each spawned obstacle and now spawn multiple at once per difficulty. The spawner will not spawn an obstacle in the same position as the last spawned obstacle.

--- TrumpetHero/Assets/Scripts/ObstacleSpawner.cs
+++ TrumpetHero/Assets/Scripts/ObstacleSpawner.cs
@@ -7,19 +7,24 @@ public class ObstacleSpawner : MonoBehaviour
     public GameObject obstacle;
     public GameObject[] obstacleNotes;
     public int note;
+    public int spawnedNote;
     public float spawnPosX;
     public float spawnPosY;
     public bool spawned = false;
-    // Start is called before the first frame update
+    public float spawnTime = 0;
+    public float difficultyTimer = 12;
+    public float easy = 3f;
+    public float medium = 2f;
+    public float hard = 1f;
+
     void Start()
     {
-
+        spawnTime = easy;
+        StartCoroutine(IncreaseDifficulty());
     }
-
-    // Update is called once per frame
     void Update()
     {
-        if (!spawned)
+        if(!spawned)
         {
             StartCoroutine(Spawn());
         }
@@ -27,11 +32,41 @@ public class ObstacleSpawner : MonoBehaviour
     private IEnumerator Spawn()
     {
         spawned = true;
-        yield return new WaitForSecondsRealtime(2);
-        note = Random.Range(0, 13);
+        yield return new WaitForSecondsRealtime(spawnTime);
+        getSpawnPos();
+        Instantiate(obstacle, new Vector3(spawnPosX, spawnPosY), Quaternion.identity);
+        if(spawnTime == medium)
+        {
+            getSpawnPos();
+            Instantiate(obstacle, new Vector3(spawnPosX, spawnPosY), Quaternion.identity);
+        }
+        if(spawnTime == hard)
+        {
+            getSpawnPos();
+            Instantiate(obstacle, new Vector3(spawnPosX, spawnPosY), Quaternion.identity);
+            getSpawnPos();
+            Instantiate(obstacle, new Vector3(spawnPosX, spawnPosY), Quaternion.identity);
+        }
+        spawned = false;
+    }
+
+    private IEnumerator IncreaseDifficulty()
+    {
+        yield return new WaitForSecondsRealtime(difficultyTimer);
+        spawnTime = medium;
+        yield return new WaitForSecondsRealtime(difficultyTimer);
+        spawnTime = hard;
+    }
+
+    private void getSpawnPos()
+    {
+        do
+        {
+            note = Random.Range(0, 13);
+        } while (note == spawnedNote);
+
+        spawnedNote = note;
         spawnPosX = obstacleNotes[note].transform.position.x;
         spawnPosY = obstacleNotes[note].transform.position.y;
-        Instantiate(obstacle, new Vector3(spawnPosX, spawnPosY), Quaternion.identity);
-        spawned = false;
     }
 }
