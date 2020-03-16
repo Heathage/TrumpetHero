514d728d214e2f84d42748744a029fad19ea95ab
James Heath <jh230155@falmouth.ac.uk>
2020-03-16 16:52:32 +0000

    The player now gets destroyed when they hit an obstacle. The Obstacle also gets destroyed in the collision.

--- TrumpetHero/Assets/Scripts/ObstacleDespawner.cs
+++ TrumpetHero/Assets/Scripts/ObstacleDespawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ObstacleDespawner : MonoBehaviour
 {
-    void OnCollisionEnter2D(Collision2D Obstacle)
+    void OnTriggerEnter2D(Collider2D Obstacle)
     {
         Destroy(Obstacle.gameObject);
     }
