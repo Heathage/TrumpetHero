c9e0c44ab791f9164a355fba4836196e6e89dfc1
James Heath <jh230155@falmouth.ac.uk>
2020-04-01 13:01:23 +0100

    Added more instructions to the help menu.

--- TrumpetHero/Assets/Scripts/Trumpet Controls/DeathTrumpetCon.cs
+++ TrumpetHero/Assets/Scripts/Trumpet Controls/DeathTrumpetCon.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO.Ports;
+using UnityEngine.SceneManagement;
 
 public class DeathTrumpetCon : MonoBehaviour
 {
-    // Start is called before the first frame update
+    SerialPort sp = new SerialPort("COM3", 9600);
+
     void Start()
     {
-        
+        sp.Open();
+        sp.ReadTimeout = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (sp.IsOpen)
+        {
+            try
+            {
+                menuControl(sp.ReadByte());
+                print(sp.ReadByte());
+            }
+            catch (System.Exception)
+            {
+
+            }
+        }
+    }
+
+    void menuControl(int Valve)
+    {
+        if (Valve == 1)
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        if (Valve == 2)
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 }
--- TrumpetHero/Assets/Scripts/Trumpet Controls/MenuTrumpetCon.cs
+++ TrumpetHero/Assets/Scripts/Trumpet Controls/MenuTrumpetCon.cs
@@ -1,8 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO.Ports;
 
 public class MenuTrumpetCon : MonoBehaviour
 {
+    SerialPort sp = new SerialPort("COM3", 9600);
+
+    void Start()
+    {
+        sp.Open();
+        sp.ReadTimeout = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (sp.IsOpen)
+        {
+            try
+            {
+                menuControl(sp.ReadByte());
+                print(sp.ReadByte());
+            }
+            catch (System.Exception)
+            {
+
+            }
+        }
+    }
+
+    public void menuControl(int Valve)
+    {
+        if (Valve == 1)
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        if (Valve == 2)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
 
 }
